da454911a711cf975ebdaefb876a43016c4c3ef5
kt <kt4and@gmail.com>
2025-10-15 14:46:33 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
   optimizeDeps: {
     exclude: ['@sqlite.org/sqlite-wasm'],
   },
